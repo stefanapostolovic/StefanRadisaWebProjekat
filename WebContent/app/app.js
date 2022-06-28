63a8e1db54c58ba23dd6afd29e9cc0e02f9cdfb8
Radiša Stojkić <stojkic.ra84.2019@uns.ac.rs>
2022-06-28 06:11:19 +0200

    Dodat prikaz jednog objekta

--- WebContent/app/app.js
+++ WebContent/app/app.js
@@ -5,6 +5,7 @@ const Registracija = { template: '<registracija></registracija>' }
 const Profil = { template: '<profil></profil>' }
 const Korisnici = { template: '<users></users>' }
 const Radnici = { template: '<addMenager></addMenager>' }
+const Facility={template:'<facility></facility>'}
 
 const router = new VueRouter({
 	mode: 'hash',
@@ -15,8 +16,12 @@ const router = new VueRouter({
 		{ path: '/profil', component: Profil},
 		{ path: '/users', component: Korisnici},
 		{ path: '/radnici', component: Radnici},
+		{ path: '/facility', component: Facility},
 ]
 });
+
+var pom ={"id":null, "name":null, "objectType":null, "status":null,"location":null, "image":null, "averageRating":null, "startTime":null, "endTime":null};
+
 
 var app = new Vue({
 	router,
