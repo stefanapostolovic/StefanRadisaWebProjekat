47b521abbd6b6586413d0deb6d74bc9b5356ce16
Radiša Stojkić <stojkic.ra84.2019@uns.ac.rs>
2022-07-09 04:03:28 +0200

    Dodat je DAO za zakazivanje treninga

--- src/beans/ScheduledTraining.java
+++ src/beans/ScheduledTraining.java
@@ -1,20 +1,45 @@
 package beans;
 
 public class ScheduledTraining {
+	private String id;
 	private String applicationDateTime;
 	private Training training;
 	private User user;
 	private User coach;
 	private String endDataTime;
-	public ScheduledTraining(String applicationDateTime, Training training, User user, User coach, String endDataTime) {
+	private boolean isDelate;
+	public ScheduledTraining(String applicationDateTime, Training training, User user, User coach, String endDataTime,String id) {
 		super();
 		this.applicationDateTime = applicationDateTime;
 		this.training = training;
 		this.user = user;
 		this.coach = coach;
 		this.endDataTime = endDataTime;
+		this.isDelate = false;
+		this.id = id;
 	}
 	
+	
+	public String getId() {
+		return id;
+	}
+
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+
+	public boolean isDelate() {
+		return isDelate;
+	}
+
+
+	public void setDelate(boolean isDelate) {
+		this.isDelate = isDelate;
+	}
+
+
 	private ScheduledTraining() {}
 
 	public String getApplicationDateTime() {
